4d1c2083f578dd0c726fa836342d25113e2b16bb
wukai <wukai@corp.netease.com>
2014-04-11 22:15:12 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "dev" ]; then
+    cat vimrc.basic > ~/.vimrc
+    cat vimrc.dev >> ~/.vimrc
+    if [ -d ~/.vim ]; then
+        cp -R vim/ ~/.vim/
+    else
+        cp -R vim ~/.vim
+    fi
+elif [ "$1" = "product" ]; then
+    cat vimrc.basic > ~/.vimrc
+    cat vimrc.product >> ~/.vimrc
+    if [ -d ~/.vim ]; then
+        cp -R vim/ ~/.vim/
+    else
+        cp -R vim ~/.vim
+    fi
+elif [ "$1" = "root" ]; then
+    cat vimrc.basic > ~/.vimrc
+    cat vimrc.root >> ~/.vimrc
+fi
