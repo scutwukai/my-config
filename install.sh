9a003df945b573bafa7fda345cbeaf777bf0c0b4
wukai <wukai@corp.netease.com>
2014-04-15 10:39:57 +0800

    improve install, allow set charset

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 if [ "$1" = "dev" ]; then
     cat vimrc.basic > ~/.vimrc
+
+    echo "" >> ~/.vimrc
     cat vimrc.dev >> ~/.vimrc
+
     if [ -d ~/.vim ]; then
         cp -R vim/ ~/.vim/
     else
@@ -10,7 +13,10 @@ if [ "$1" = "dev" ]; then
     fi
 elif [ "$1" = "product" ]; then
     cat vimrc.basic > ~/.vimrc
+
+    echo "" >> ~/.vimrc
     cat vimrc.product >> ~/.vimrc
+
     if [ -d ~/.vim ]; then
         cp -R vim/ ~/.vim/
     else
@@ -18,5 +24,10 @@ elif [ "$1" = "product" ]; then
     fi
 elif [ "$1" = "root" ]; then
     cat vimrc.basic > ~/.vimrc
+
+    echo "" >> ~/.vimrc
     cat vimrc.root >> ~/.vimrc
 fi
+
+echo "" >> ~/.vimrc
+echo "set termencoding=$2" >> ~/.vimrc
